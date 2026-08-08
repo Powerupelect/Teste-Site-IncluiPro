0f140369419ec74e550ee52f9c3b38133c75d19b
Ester <esterlima255@outlook.com>
2026-08-08 18:47:46 -0300

    Delete IncluiPro Site/incluiprosourcev5code directory

--- IncluiPro Site/incluiprosourcev5code/src/components/AdminProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { getAdminToken } from '../lib/adminAuth.js'
-
-export function AdminProtectedRoute({ children }) {
-  if (!getAdminToken()) {
-    return <Navigate to="/admin/login" replace />
-  }
-  return children
-}
